484d153758ca4fcb9cab2ebdc63ff56133501963
uvchuk <uvchuk@gmail.com>
2023-03-26 19:38:52 +0200

    initial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <BrowserRouter basename="/goit-react-hw-05-movies">
+      <Routes>
+        <Route path="/" />
+      </Routes>
+    </BrowserRouter>
   );
 };
